78f4c46a5947e3dbc752c18030d09668bcdf5a00
iamara <iamarabarrios457@gmail.com>
2023-08-10 07:26:26 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+#define MAX_COMMAND_LENGTH 1024
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+void display_prompt();
+char *read_command();
+void execute_command(char *command);
+extern char **environ;
+
+#endif
