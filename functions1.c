d489e576c3b73be0d84277f2f2509f8ec7d94651
iamara <iamarabarrios457@gmail.com>
2023-08-12 17:18:02 -0700

    functions1.c

---
+++ functions1.c
@@ -0,0 +1,64 @@
+#include "main.h"
+/**
+ *display_prompt - print symbol $
+ */
+void display_prompt(void)
+{
+	fprintf(stdout, "$ ");
+}
+
+#define MAX_ARGUMENTS 64
+
+/**
+ *read_command - read command  write for the user
+ *
+ *Return: line or NULL
+ */
+char *read_command()
+{
+	char *line = NULL;
+	size_t bufsize = 0;
+	ssize_t characters_read;
+
+	characters_read = getline(&line, &bufsize, stdin);
+	if (characters_read == -1)
+	{
+		free(line);
+		return (NULL);
+	}
+	line[strcspn(line, "\n")] = '\0';
+
+	return (line);
+}
+/**
+*pased_arguments - pasa argumento
+*@line: linea pasada
+*Return: argumento pasado
+*/
+char **pased_arguments(char *line)
+{
+	char **arguments = malloc(MAX_ARGUMENTS * sizeof(char *));
+	char *token = strtok(line, " ");
+	int arg_index = 0;
+
+	if (!arguments)
+	{
+		perror("error memory allocation");
+		exit(EXIT_FAILURE);
+	}
+	while (token != NULL)
+	{
+		arguments[arg_index] = strdup(token);
+		arg_index++;
+
+		if (arg_index >= MAX_ARGUMENTS - 1)
+		{
+			fprintf(stderr, "too many arguments\n");
+			break;
+		}
+
+		token = strtok(NULL, " ");
+	}
+	arguments[arg_index] = NULL;
+	return (arguments);
+}
