1749436d65d2f5ceca1c65bf8827f14a688d9a89
iamara <iamarabarrios457@gmail.com>
2023-08-14 14:56:48 -0700

    main.h

--- main.h
+++ main.h
@@ -10,11 +10,11 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 
-void display_prompt();
+void display_prompt(void);
 char *read_command();
 char **pased_arguments(char *line);
 void execute_command(char *command, char **arguments);
-void print_environment();
+void print_environment(void);
 char *find_executable_path(const char *command, const char *path);
 char *recreated_getenv(const char *name);
 ssize_t getline(char **lineptr, size_t *n, FILE *stream);
